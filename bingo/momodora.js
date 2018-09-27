400bb51c5a25a06a611140c9dd2334c001099d12
Michael H <hellercmichael@gmail.com>
2018-09-26 21:48:52 -0400

    Initial commit.

---
+++ bingo/momodora.js
@@ -0,0 +1,401 @@
+var bingoList = [];
+
+bingoList[1] = [{
+  "name": "Make cat form Kaho sleep during a boss fight",
+  "types": ["cat-form"]
+}, {
+  "name": "Make Kaho Yawn during a boss fight",
+  "types": []
+}, {
+  "name": "Defeat Edea only with arrows",
+  "types": ["arrows", "edea", "restriction"]
+}, {
+  "name": "Hit 5 Imp projectiles out of the air with your leaf",
+  "types": ["projectile"]
+}];
+
+bingoList[2] = [{
+  "name": "Defeat Edea by only attacking her face",
+  "types": ["restriction", "edea"]
+}, {
+  "name": "Kill the great big turnip in the Grove",
+  "types": ["enemies"]
+}, {
+  "name": "Perform the quick-kill on Edea",
+  "types": ["arrows", "edea"]
+}, {
+  "name": "Obtain Edea's Boss Item",
+  "types": ["boss-items", "edea", "require-no-hit"]
+}];
+
+bingoList[3] = [{
+  "name": "Defeat Frida only with arrows",
+  "types": ["arrows", "restriction", "frida"]
+}, {
+  "name": "No use of the Konami Fairy allowed",
+  "types": ["fairy", "restriction"]
+}, {
+  "name": "Watch an Imp jump into a pit/spikes",
+  "types": [""]
+}, {
+  "name": "Obtain all 2 Ivory Bugs in Karst City",
+  "types": ["collecting", "karst-city"]
+}];
+
+bingoList[4] = [{
+  "name": "Obtain all 2 Vitality Fragments in Karst City",
+  "types": ["collecting", "karst-city"]
+}, {
+  "name": "Defeat Soldier miniboss without using Passive or Active Items",
+  "types": ["restriction"]
+}, {
+  "name": "Obtain Frida's Boss Item",
+  "types": ["boss-items", "frida", "require-no-hit"]
+}, {
+  "name": "Obtain all 1 Vitality Fragments in the Subterranean Grave",
+  "types": ["collecting"]
+}];
+
+bingoList[5] = [{
+  "name": "Kill all enemies in the grove",
+  "types": ["enemies"]
+}, {
+  "name": "Get through Grove without killing any Imps",
+  "types": [""]
+}, {
+  "name": "Defeat Frida without using any Passive or Active Items",
+  "types": ["restriction", "frida"]
+}, {
+  "name": "Destroy 12 of the balls in Moka's snowflake attack before it disappears",
+  "types": [""]
+}];
+
+bingoList[6] = [{
+  "name": "Open no chests",
+  "types": ["chests", "restriction"]
+}, {
+  "name": "Kill 10 Bats",
+  "types": ["enemies"]
+}, {
+  "name": "Obtain Fennel's Boss Item",
+  "types": ["boss-items", "fennel", "require-no-hit"]
+}, {
+  "name": "Give the Dirty Shroom to the Imp Girl",
+  "types": ["quest"]
+}, {
+  "name": "Enter Whiteleaf Memorial Park without the Garden Key",
+  "types": ["shortcut"]
+}];
+
+bingoList[7] = [{
+  "name": "Meet Eri outside the cursed city",
+  "types": ["quest"]
+}, {
+  "name": "Obtain all 2 Vitality Fragments in the Cinder Chambers",
+  "types": ["collecting"]
+}, {
+  "name": "Buy the Ring of Candor and keep it equipped for the rest of the run",
+  "types": ["munny"]
+}, {
+  "name": "Visit all 7 Shop Vendors",
+  "types": ["quest"]
+}, {
+  "name": "Hit Arsonist with all of the exploding barrels",
+  "types": ["arsonist"]
+}];
+
+bingoList[8] = [{
+  "name": "Climb up the looooooooong ladder without jumping",
+  "types": ["monastery"]
+}, {
+  "name": "Kill 10 of Frida's dogs",
+  "types": ["frida"]
+}, {
+  "name": "Obtain all 1 Vitality Fragments in Whiteleaf Memorial Park",
+  "types": ["collecting"]
+}, {
+  "name": "Obtain all 3 Ivory Bugs in the Cinder Chambers",
+  "types": ["collecting"]
+}, {
+  "name": "Kill 10 Seedlings in Whiteleaf Memorial Park without getting poisoned",
+  "types": ["enemies"]
+}];
+
+bingoList[9] = [{
+  "name": "Defeat Arsonist without jumping",
+  "types": ["arsonist"]
+}, {
+  "name": "Defeat Frida by only hitting them in cat form",
+  "types": ["cat-form", "frida", "restriction"]
+}, {
+  "name": "Donate over 200 Munny to Saint Esselin",
+  "types": ["monastery", "munny"]
+}, {
+  "name": "Obtain all 3 Ivory Bugs in the Whiteleaf Memorial Park",
+  "types": ["collecting"]
+}, {
+  "name": "Do not use the Bakman shortcut (next to Frida) out of the Subterranean Graves",
+  "types": ["shortcut"]
+}];
+
+bingoList[10] = [{
+  "name": "Obtain all 1 Vitality Fragments in the Forlorn Monastery ",
+  "types": ["collecting", "monastery"]
+}, {
+  "name": "Keep the Adorned Ring or Medal of Equivalence equipped for the entire run",
+  "types": ["difficulty"]
+}, {
+  "name": "Aggro the witch before Frida and finish the fight without killing her",
+  "types": ["enemies", "frida"]
+}, {
+  "name": "Defeat Arsonist only with arrows",
+  "types": ["arsonist", "arrows", "restriction"]
+}, {
+  "name": "Obtain all 1 Vitality Fragments in Frore Ciele",
+  "types": ["collecting", "frore"]
+}];
+
+bingoList[11] = [{
+  "name": "Hit the big bell in the Forlorn Monastery",
+  "types": ["monastery"]
+}, {
+  "name": "Obtain Lubella 1's Boss Item",
+  "types": ["boss-items", "lubella1", "bakman", "require-no-hit"]
+}, {
+  "name": "Obtain all 1 Ivory Bugs in the Subterranean Grave",
+  "types": ["collecting"]
+}, {
+  "name": "Defeat Arsonist by only hitting them in cat form",
+  "types": ["arsonist", "cat-form", "restriction"]
+}, {
+  "name": "Defeat Soldier miniboss by only hitting them in cat form",
+  "types": ["soldier", "cat-form", "restriction"]
+}];
+
+bingoList[12] = [{
+  "name": "Obtain all 3 Vitality Fragments in the Sacred Ordalia Grove",
+  "types": ["collecting"]
+}, {
+  "name": "Obtain all 4 Ivory Bugs at the Sacred Ordalia Grove",
+  "types": ["collecting"]
+}, {
+  "name": "Meet Askorn at his wife's grave",
+  "types": ["dialogue", "quest"]
+}, {
+  "name": "Obtain 500 Munny",
+  "types": ["chests", "munny"]
+}, {
+  "name": "No rolljumping allowed",
+  "types": ["restriction"]
+}];
+
+bingoList[13] = [{
+  "name": "Poison yourself with your own arrow during a boss fight, finish the fight and save",
+  "types": ["arrow", "boss-items", "require-no-hit"]
+}, {
+  "name": "Kill the creepy mage dude that guards the Tainted Missive",
+  "types": ["enemies"]
+}, {
+  "name": "Shoot a knife out of the air with an arrow",
+  "types": ["projectile"]
+}, {
+  "name": "Obtain all 2 Ivory Bugs in the Forlorn Monastery",
+  "types": ["collecting", "monastery"]
+}, {
+  "name": "Defeat Lubella 1 without using any Passive or Active Items",
+  "types": ["lubella1", "restriction"]
+}, {
+  "name": "Obtain 9 Vitality Fragments",
+  "types": ["collection"]
+}];
+
+bingoList[14] = [{
+  "name": "Obtain 10 Ivory Bugs",
+  "types": ["collecting"]
+}, {
+  "name": "Defeat Fennel without using any Passive or Active Items",
+  "types": ["fennel", "restriction"]
+}, {
+  "name": "Kill Fennel before she goes into her second phase",
+  "types": ["fennel"]
+}, {
+  "name": "No cat form unless it's mandatory (ie: cat section, small tunnels)",
+  "types": ["cat-form"]
+}];
+
+bingoList[15] = [{
+  "name": "Hit 14 different Bells with Leaf/Arrow",
+  "types": ["quest", "arrow"]
+}, {
+  "name": "Defeat Lubella 2 without using any Passive or Active Items",
+  "types": ["lubella2", "restriction"]
+}, {
+  "name": "Get cursed by Fennel, finish the fight and save",
+  "types": ["curse", "fennel", "require-no-hit"]
+}, {
+  "name": "Make Kaho invisible",
+  "types": [""]
+}];
+
+bingoList[16] = [{
+  "name": "Defeat Fennel by only hitting them in cat form",
+  "types": ["fennel", "cat-form", "restriction"]
+}, {
+  "name": "Get through Grove without getting hit",
+  "types": ["require-no-hit", "restriction"]
+}, {
+  "name": "Do not die",
+  "types": [""]
+}, {
+  "name": "Play on Hard difficulty",
+  "types": ["restriction"]
+}, {
+  "name": "No dashing allowed",
+  "types": ["restriction"]
+}, {
+  "name": "Don't kill any common enemies",
+  "types": ["enemies"]
+}];
+
+bingoList[17] = [{
+  "name": "Defeat Frida without arrows",
+  "types": ["arrows", "restriction", "frida"]
+}, {
+  "name": "Obtain Lubella 2's Boss Item",
+  "types": ["boss-items", "lubella2", "require-no-hit"]
+}, {
+  "name": "Defeat Fairy miniboss without using Passive or Active Items",
+  "types": ["fairyboss", "restriction"]
+}, {
+  "name": "Equip the Black Satchet for a whole boss fight",
+  "types": ["munny"]
+}, {
+  "name": "Defeat Fennel only with arrows",
+  "types": ["fennel", "arrows", "restriction"]
+}, {
+  "name": "Listen to the hidden song in the Pinacotheca",
+  "types": [""]
+}];
+
+bingoList[18] = [{
+  "name": "Obtain all 3 Ivory Bugs in the Royal Pinacothea",
+  "types": ["collecting"]
+}, {
+  "name": "End it all with the Necklace of Sacrifice on",
+  "types": ["munny"]
+}, {
+  "name": "Defeat Archpriestess Choir",
+  "types": ["quest", "choir"]
+}, {
+  "name": "Obtain the Fresh Spring Leaf",
+  "types": [""]
+}];
+
+bingoList[19] = [{
+  "name": "Get cursed by Moka, finish the fight and save",
+  "types": ["curse", "lupiar1", "require-no-hit", "shortcut"]
+}, {
+  "name": "Inspect the red skull in the Pinacotheca",
+  "types": [""]
+}, {
+  "name": "Summon the fairy in the room with the fairy code",
+  "types": ["fairy", "enemies"]
+}, {
+  "name": "Obtain all 3 Vitality Fragments in the Royal Pinacotheca",
+  "types": ["collection"]
+}];
+
+bingoList[20] = [{
+  "name": "Obtain Lupiar's Boss Item",
+  "types": ["boss-items", "lupiar", "require-no-hit"]
+}, {
+  "name": "Defeat Lupiar and Magnolia by only hitting them in cat form",
+  "types": ["lupiar", "cat-form", "restriction"]
+}, {
+  "name": "Finish a boss with sealed wind",
+  "types": ["sealed wind"]
+}, {
+  "name": "Defeat Lupiar without using any Passive or Active Items",
+  "types": ["lupiar", "restriction"]
+}, {
+  "name": "Give the Hazel Badge to Eri",
+  "types": ["quest", "collecting"]
+}, {
+  "name": "Obtain Archpriestess Choir's Boss Item",
+  "types": ["boss-items", "choir", "require-no-hit"]
+}];
+
+bingoList[21] = [{
+  "name": "Kill every enemy that enters the screen (even when you re-enter rooms)",
+  "types": ["enemies"]
+}, {
+  "name": "Do not get hit by physical attacks or projectiles",
+  "types": ["require-no-hit", "restriction"]
+}];
+
+bingoList[22] = [{
+  "name": "Release the draw-bridge to Karst Castle",
+  "types": ["karst-castle"]
+}, {
+  "name": "Talk to the cat in Karst Castle",
+  "types": ["karst-castle"]
+}, {
+  "name": "Open all illusory walls",
+  "types": ["illusory-walls", "quest"]
+}, {
+  "name": "Obtain all 10 Bellflowers",
+  "types": ["quest", "bellflower", "collecting"]
+}, {
+  "name": "Open all 14 Chests",
+  "types": ["chests", "munny", "quest"]
+}];
+
+bingoList[23] = [{
+  "name": "Obtain all 20 Ivory Bugs",
+  "types": ["collecting"]
+}, {
+  "name": "Obtain all 2 Ivory Bugs in Karst Castle",
+  "types": ["collecting", "karst-castle"]
+}, {
+  "name": "Obtain all 17 Vitality Fragments",
+  "types": ["collection"]
+}, {
+  "name": "Obtain all 3 Vitality Fragments in Karst Castle",
+  "types": ["collecting", "karst-castle"]
+}, {
+  "name": "Defeat Fairy miniboss by only hitting them in cat form",
+  "types": ["fairyboss", "cat-form", "restriction"]
+}, {
+  "name": "Open the Karst Castle shortcut with your leaf",
+  "types": ["shortcut", "karst-castle"]
+}];
+
+bingoList[24] = [{
+  "name": "Listen to Kath's story after Arsonist and kill her at the end",
+  "types": ["quest"]
+}, {
+  "name": "Listen to Kath's story after Arsonist and save her at the end",
+  "types": ["quest"]
+}, {
+  "name": "Ride the Karst Castle elevator up and down",
+  "types": ["karst-castle"]
+}];
+
+bingoList[25] = [{
+  "name": "Go through an entire cycle of phase 3 Queen attacks before fighting her",
+  "types": ["queen", "restriction"]
+}, {
+  "name": "Defeat Phase 1 of Queen by only hitting them in cat form",
+  "types": ["queen", "cat-form", "restriction"]
+}, {
+  "name": "Defeat Phase 1 of Queen without using any Passive or Active Items",
+  "types": ["queen", "restriction"]
+}, {
+  "name": "Defeat Phase 1 of Queen only with arrows",
+  "types": ["queen", "arrows", "restriction"]
+}, {
+  "name": "Get 80% Map Completion",
+  "types": ["map"]
+}];
+
+$(function() { srl.bingo(bingoList, 5); });
